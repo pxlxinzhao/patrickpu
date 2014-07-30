3283522c0ea66282ba2b91ee88270a30a7367ec0
Patrick Pu <pxlxinzhao@gmail.com>
2014-07-30 14:31:49 -0400

    SpringMVCTrackingService first commit

---
+++ Spring4MVCHelloWorld/src/main/java/com/javahash/spring/controller/HelloWorldController.java
@@ -0,0 +1,24 @@
+package com.javahash.spring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class HelloWorldController { 
+
+    @RequestMapping("/hello")
+    public String hello(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "helloworld";
+    }
+    
+    @RequestMapping("/map")
+    public String mapping(@RequestParam(value="lat", required=false, defaultValue="35.40") String lat,
+    					  @RequestParam(value="lon", required=false, defaultValue="139.45") String lon, Model model) {
+        model.addAttribute("lat", lat);
+        model.addAttribute("lon", lon);
+        return "showmap";
+    }
+}
---
+++ Spring4MVCHelloWorld/src/main/webapp/index.jsp
@@ -0,0 +1,33 @@
+<html>
+
+<head>
+<style>
+div {margin-top: 50px;
+	 margin-left: auto;
+	 margin-right: auto;
+	 padding-top: 1px;
+	 text-align: center; 
+	 width:50%;
+	 background-color: #AD8533;
+	}
+table { margin-left: auto;
+	    margin-right: auto;	
+	  }
+th {padding: 20px}
+</style>
+
+</head>
+
+<body>
+
+<div>
+	<h1 align="center">Hello World</h1>
+	<table>
+		<tr>
+			<th><a href="http://localhost:8080/Spring4MVCHelloWorld/hello.do" > Hello World Example </a></th>
+			<th><a href="http://localhost:8080/Spring4MVCHelloWorld/map.do" > Tracking Service</a></th>
+		</tr>
+	</table>
+</div>
+</body>
+</html>
